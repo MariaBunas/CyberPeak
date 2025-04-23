5894ac6ade623f60077b1d9424fcd21282cf28f9
MariaBunas <maria.bunas@e-cdloga.ro>
2025-04-23 17:20:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//
+document.addEventListener("DOMContentLoaded", function () {
+    let video = document.getElementById("earthVideo");
+    setTimeout(() => {
+        video.style.transition = "opacity 2s";
+        video.style.opacity = 0;
+        setTimeout(() => {
+            video.style.display = "none";
+            document.getElementById("map").style.display = "block";
+        }, 2000);
+    }, 5000);
+});
+//
 
 const map = L.map('map').setView([45.9432, 24.9668], 6);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
