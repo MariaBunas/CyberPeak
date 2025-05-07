22a1c9ea446f84179648d1c895b146770131c4fa
MariaBunas <maria.bunas@e-cdloga.ro>
2025-05-08 02:10:28 +0300

    Update myGAPIscript.js

--- myGAPIscript.js
+++ myGAPIscript.js
@@ -1,20 +1,20 @@
 const API_KEY = "AIzaSyBE4jeVMYnAio8DgHU8EudkIJyA_M3odFU"; // Replace with your actual API key
 
-async function getFileId_old(fileName, extension) {
-    // const url = `https://www.googleapis.com/drive/v3/files?q=name='${fileName}' and trashed=false&key=${API_KEY}&fields=files(id,name)`;
-    // const url = `https://www.googleapis.com/drive/v3/files?q=title='${fileName}' and fileExtension='${extension}' and trashed=false&key=${API_KEY}&fields=files(id,name)`;
-    //title="File_1.xml" and fileExtension="xml"
-     const url = `https://www.googleapis.com/drive/v3/files?q=name='${fileName}'&key=${API_KEY}&fields=files(id,name)`;
-    const response = await fetch(url);
-    const data = await response.json();
+// async function getFileId_old(fileName, extension) {
+//     // const url = `https://www.googleapis.com/drive/v3/files?q=name='${fileName}' and trashed=false&key=${API_KEY}&fields=files(id,name)`;
+//     // const url = `https://www.googleapis.com/drive/v3/files?q=title='${fileName}' and fileExtension='${extension}' and trashed=false&key=${API_KEY}&fields=files(id,name)`;
+//     //title="File_1.xml" and fileExtension="xml"
+//      const url = `https://www.googleapis.com/drive/v3/files?q=name='${fileName}'&key=${API_KEY}&fields=files(id,name)`;
+//     const response = await fetch(url);
+//     const data = await response.json();
     
-    if (data.files && data.files.length > 0) {
-        return data.files[0].id; // Get first matching file ID
-    } else {
-        console.error("File not found.");
-        return null;
-    }
-}
+//     if (data.files && data.files.length > 0) {
+//         return data.files[0].id; // Get first matching file ID
+//     } else {
+//         console.error("File not found.");
+//         return null;
+//     }
+// }
 
 async function getFileId(fileName) {
     // const url = `https://www.googleapis.com/drive/v3/files?q=name='${fileName}' and trashed=false&key=${API_KEY}&fields=files(id,name)`;
